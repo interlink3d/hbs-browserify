b962c056c725e01549cbaaa417d917da57a32f79
Edgar Barajas <ebarajas05@gmail.com>
2016-08-23 10:30:02 -0500

    touched and modified initial SS files for article and HBS

--- js/main.js
+++ js/main.js
@@ -16,57 +16,6 @@ Handlebars.registerHelper("inc", function(value) {
 // attach our rendered HTML to the DOM
 $("#entryOutput").append(entryTemplate(entryData));
 
-var solarSystemData = {
-  planets: [
-    {
-      type: "rocky",
-      name: "Mercury"
-    },
-    {
-      type: "rocky",
-      name: "Venus"
-    },
-    {
-      type: "rocky",
-      name: "Earth"
-    },
-    {
-      type: "rocky",
-      name: "Mars"
-    },
-    {
-      type: "gas giant",
-      name: "Jupiter"
-    },
-    {
-      type: "gas giant",
-      name: "Saturn"
-    },
-    {
-      type: "ice giant",
-      name: "Uranus"
-    },
-    {
-      type: "ice giant",
-      name: "Neptune"
-    }
-  ]
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
